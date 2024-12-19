393f9144f2939a552cc8f005b47f0d3dbacc9cc6
Evgenii <dev-www@ya.ru>
2024-12-20 00:42:30 +0400

    test(integration): change contract

--- test/integration/ci.integration.ts
+++ test/integration/ci.integration.ts
@@ -1,17 +1,15 @@
 import { expect } from "chai";
 
-import { discover } from "../../lib/protocol/discover";
+import { MiniMeToken } from "typechain-types";
+
+import { loadContract } from "lib";
 
 describe("Fast tests for testing CI integration", () => {
   it("Check LDO amount at address 3", async () => {
-    const { contracts } = await discover();
+    const ldo = await loadContract<MiniMeToken>("MiniMeToken", "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32");
 
-    const { lido } = contracts;
+    const balance = await ldo?.balanceOf("0x0000000000000000000000000000000000000777");
 
-    const balance3 = await lido.balanceOf("0x0000000000000000000000000000000000000777");
-    const balance1 = await lido.balanceOf("0x0000000000000000000000000000000000000001");
-
-    expect(balance3).to.be.equal(0);
-    expect(balance1).to.be.equal(2433715748);
+    expect(balance).to.be.equal(10000000000000000000000n);
   });
 });
