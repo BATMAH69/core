5d931e465388d1d32e15c9a4002ff7860cb4bfc6
Evgenii <dev-www@ya.ru>
2024-12-19 19:23:19 +0400

    test(integration): add non-zero assert to check that it works

--- test/integration/ci.integration.ts
+++ test/integration/ci.integration.ts
@@ -9,7 +9,9 @@ describe("Fast tests for testing CI integration", () => {
     const { lido } = contracts;
 
     const balance3 = await lido.balanceOf("0x0000000000000000000000000000000000000777");
+    const balance1 = await lido.balanceOf("0x0000000000000000000000000000000000000001");
 
     expect(balance3).to.be.equal(0);
+    expect(balance1).to.be.equal(2433715748);
   });
 });
